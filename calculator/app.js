60c7879a08da0a067d5ab845fcb51e2482d33289
Ella Moss <emoss@campusstore.utah.edu>
2022-08-12 14:40:34 -0600

    initial commit

---
+++ calculator/app.js
@@ -0,0 +1,11 @@
+/*Here's your calculator JavaScript file, where client-side logic happens.
+ * Why not write the entire calculator in JavaScript? Mostly to give you experience with PHP ;D
+ * but you totally could do it all client-side if you wanted to. */
+
+//So, first you'll want to bind button handlers: 
+$(document).ready(function(){
+	//Everything in this block runs as soon the page is "ready" (ie, finished loading)
+	$(".calculator-button").click(function(){
+		//do something when a button is clicked
+	});
+});
